e0ef4f7e1f3d28c4eaaa0e4cb650ca3fdb1c6ed4
lrgeorge27 <lrgeorge27@gmail.com>
2018-01-31 21:58:46 +0000

    half way through sec1 level8

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("It works!");
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT); //sets port property for entire app
+
+app.get('/', function(req, res){
+   console.log("GET the homepage");
+   res
+   .status(404)
+   .send("Express yourself!");
+});
+
+app.get('/json', function(req, res){
+   console.log("GET the json");
+   res
+   .status(200)
+   .json({"jsonData": true});
+});
+
+var server = app.listen(app.get('port'), function(){  //define server
+    var port = server.address().port;   //get port address
+    console.log("Magic happens on port " + port); //asynchronous call
+});
