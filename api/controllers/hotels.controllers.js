acf3c339eb7907832f35290f8917e4856f81b0f1
lrgeorge27 <lrgeorge27@gmail.com>
2018-02-04 14:53:43 +0000

    finished sec2 level 22, data being pull from db

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,47 +1,63 @@
 //create a controller file for each logical collection of items
 var dbconn = require('../data/dbconnections.js'); //links to node, which connects to mongodb, has the same state as app.js
+var ObjectId = require('mongodb').ObjectId; //driver has a method to query mongodb using data type, objectId helper
 var hotelData = require('../data/hotel-data.json'); //link json data file for hotels
 
 module.exports.hotelsGetAll = function(req, res){
    
    var db = dbconn.get();
-   console.log("db", db);
+   var collection = db.collection('hotels');
+   
+    var offset = 0;
+    var count = 5;
+   
+    if(req.query && req.query.offset){   //if query property exist, and query property has a offset
+      offset = parseInt(req.query.offset, 10); //take value and set as offset value, use parseInt to convert string to num
+    }
+   
+    if(req.query && req.query.count){   //if query property exist, and query property has a count
+      count = parseInt(req.query.count, 10); //take value and set as count value, use parseInt to convert string to num
+    }
+
+   collection
+    .find()
+    .skip(offset)
+    .limit(count)
+    .toArray(function(err, docs){
+        console.log("Found hotels", docs);
+           res
+            .status(200)
+            .json(docs);
+    });
+   
+};      
+//   console.log("db", db);
  //opening database connection is asynchronous, no gaurantee it will be ready before controller file is brought into app.
  //a GET call on each method makes sure db connection can be made when we need it
  //best practice to open the connection once when app starts and reuse that connection whenever needed
-   console.log("GET the hotels");
-   console.log(req.query);
+//   console.log("GET the hotels");
+//   console.log(req.query);
    
-   var offset = 0;
-   var count = 5;
-   
-   if(req.query && req.query.offset){   //if query property exist, and query property has a offset
-       offset = parseInt(req.query.offset, 10); //take value and set as offset value, use parseInt to convert string to num
-   }
-   
-   if(req.query && req.query.count){   //if query property exist, and query property has a count
-       count = parseInt(req.query.count, 10); //take value and set as count value, use parseInt to convert string to num
-   }
-
-   var returnData = hotelData.slice(offset, offset+count); //take hotelData array, slice it using offset as starting point and adding count to offset to get the end point of slice
-   
-    res
-        .status(200)
-        .json(returnData);  
-};
 
 module.exports.hotelsGetOne = function(req, res){
     
     var db = dbconn.get();
-    console.log("db", db);
+    var collection = db.collection('hotels');
+    // console.log("db", db);
     
     //extract a parameter and put it into a var
     var hotelId = req.params.hotelId;
-    var thisHotel = hotelData[hotelId]; //holds into about specific hotel using url parameter as location index on hotelData array (json object[0-i])
+    // var thisHotel = hotelData[hotelId]; //holds into about specific hotel using url parameter as location index on hotelData array (json object[0-i])
     console.log("GET hotelId", hotelId);
-    res
-        .status(200)
-        .json(thisHotel);  
+    
+    collection
+        .findOne({
+            _id: ObjectId(hotelId)
+        }, function(err, doc){
+            res
+                .status(200)
+                .json(doc);  
+         });
 };
 
 module.exports.hotelsAddOne = function(req, res){
