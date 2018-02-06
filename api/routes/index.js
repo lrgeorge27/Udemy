b9fde92a93220a6b9f8b685e8c7a04fa9b29ac38
lrgeorge27 <lrgeorge27@gmail.com>
2018-02-06 16:38:30 +0000

    finished sec2 level 29, added reviews routes and pulled reviews array from MongoDB using Mongoose

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();  //Instantiate express router
+
 var ctrlHotels = require('../controllers/hotels.controllers.js');
+var ctrlReviews = require('../controllers/reviews.controllers.js');
+
 router
     .route('/hotels')
     .get(ctrlHotels.hotelsGetAll); //map controller to route in hotels.controllers
@@ -19,5 +22,14 @@ router
     //         .status(200)
     //         .json({"jsonData": "POST received"});
     // });
+    
+//Review routes
+router
+    .route('/hotels/:hotelId/reviews')
+    .get(ctrlReviews.reviewsGetAll); //map controller to route in hotels.controllers
+
+router
+    .route('/hotels/:hotelId/reviews/:reviewId') //adding a parameter, controller can access
+    .get(ctrlReviews.reviewsGetOne); //map controller to route in hotels.controllers
 
 module.exports = router;    //export
