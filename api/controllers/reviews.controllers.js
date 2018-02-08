c9b6a5185295c091f14c809652bdf23b1fa861e7
lrgeorge27 <lrgeorge27@gmail.com>
2018-02-08 17:04:17 +0000

    finished sec3 lecture 34, delete method for single hotel and review

--- api/controllers/reviews.controllers.js
+++ api/controllers/reviews.controllers.js
@@ -139,5 +139,114 @@ module.exports.reviewsAddOne = function(req, res){
 };
 
 module.exports.reviewsUpdateOne = function(req, res){
-  
+    var hotelId = req.params.hotelId;
+    var reviewId = req.params.reviewId;
+    console.log('PUT reviewId ' + reviewId + ' for hotelId ' + hotelId);
+    
+    Hotel
+        .findById(hotelId)
+        .select("reviews") 
+        .exec(function(err, hotel){
+            var thisReview;
+            var response = {
+                status: 200,
+                message: hotel
+            };
+            if(err){
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            } 
+            else if(!hotel) {
+                response.status = 404;
+                response.message = {"message": "Hotel ID not found"};
+            }else{
+                //Get the review
+                thisReview = hotel.reviews.id(reviewId);
+                //if the review doesn't exist in Mongoose
+                if(!thisReview){
+                    response.status = 404;
+                    response.message = {"message": "Review ID not found" + reviewId};
+                }
+            }
+            if (response.status !== 200){
+                res
+                    .status(response.status)
+                    .json(response.message); 
+            } else {
+                //update data
+               thisReview.name = req.body.name;
+               thisReview.rating = parseInt(req.body.rating, 10);
+               thisReview.review = req.body.review;
+                //save model instance to mongoDB
+                hotel.save(function(err, hotelUpdated){
+               if(err){
+                   res 
+                     .status(500)
+                     .json(err);
+               } else {
+                   res
+                     .status(204) //success, no content
+                     .json();
+               }
+            });
+            }
+         });
+
+};
+
+module.exports.reviewsDeleteOne = function(req, res){
+     var hotelId = req.params.hotelId;
+    var reviewId = req.params.reviewId;
+    console.log('PUT reviewId ' + reviewId + ' for hotelId ' + hotelId);
+    
+    Hotel
+        .findById(hotelId)
+        .select("reviews") 
+        .exec(function(err, hotel){
+            var thisReview;
+            var response = {
+                status: 200,
+                message: hotel
+            };
+            if(err){
+                console.log("Error finding hotel");
+                response.status = 500;
+                response.message = err;
+            } 
+            else if(!hotel) {
+                response.status = 404;
+                response.message = {"message": "Hotel ID not found"};
+            }else{
+                //Get the review
+                thisReview = hotel.reviews.id(reviewId);
+                //if the review doesn't exist in Mongoose
+                if(!thisReview){
+                    response.status = 404;
+                    response.message = {"message": "Review ID not found" + reviewId};
+                }
+            }
+            if (response.status !== 200){
+                res
+                    .status(response.status)
+                    .json(response.message); 
+            } else {
+                //delete the review
+                hotel.reviews.id(reviewId).remove();
+                //save changes to model instance to mongoDB
+                hotel.save(function(err, hotelUpdated){
+               if(err){
+                   res 
+                     .status(500)
+                     .json(err);
+               } else {
+                   res
+                     .status(204) //success, no content
+                     .json();
+               }
+            });
+            }
+         });
+
+    
 };
--- api/routes/index.js
+++ api/routes/index.js
@@ -13,7 +13,8 @@ router
 router
     .route('/hotels/:hotelId') //adding a parameter, controller can access
     .get(ctrlHotels.hotelsGetOne) //map controller to route in hotels.controllers
-    .put(ctrlHotels.hotelsUpdateOne); //method to update a single hotel doc, put updates entire doc, patch does partial updates
+    .put(ctrlHotels.hotelsUpdateOne) //method to update a single hotel doc, put updates entire doc, patch does partial updates
+    .delete(ctrlHotels.hotelsDeleteOne); 
 
 //Review routes
 router
@@ -24,6 +25,7 @@ router
 router
     .route('/hotels/:hotelId/reviews/:reviewId') //adding a parameter, controller can access
     .get(ctrlReviews.reviewsGetOne) //map controller to route in hotels.controllers
-    .put(ctrlReviews.reviewsUpdateOne); //method to update single review doc with a single hotel doc
+    .put(ctrlReviews.reviewsUpdateOne) //method to update single review doc with a single hotel doc
+    .delete(ctrlReviews.reviewsDeleteOne);
 
 module.exports = router;    //export
